33a932c02ce3c297f4ab38bb49f975d74c19f7fc
ashmeet-singh <76838543+ashmeet-singh@users.noreply.github.com>
2021-01-05 14:17:26 +0530

    Changed Engine

--- public/js/Engine.js
+++ public/js/Engine.js
@@ -3,119 +3,49 @@ var Engine = {};
 (function () {
 
     Engine.create = function () {
+
         var engine = {
             frameRequestId: undefined,
-            sceneSwitch: undefined,
-            scenes: {},
-            storage: {}
+            time: undefined,
+            ms_per_update: 1000 / 60
         };
-
-        Engine.switchScene(engine, 'scene');
 
         return engine;
     };
 
-    Engine.update = function (engine, ms_per_update) {
-        var sceneSwitch = engine.sceneSwitch,
-            scene = engine.scenes[sceneSwitch],
-            timing = scene.timing;
+    Engine.start = function (engine) {
+        if (engine.time === undefined) { engine.time = Date.now(); }
 
-        if (timing.previous === undefined) { timing.previous = Date.now(); }
-        timing.current = Date.now();
-        timing.elapsed = timing.current - timing.previous;
-        if (timing.lag === undefined) { timing.lag = 0; }
-        timing.lag += timing.elapsed;
-        if (ms_per_update === undefined) { timing.ms_per_update = 1000 / 60; }
-        else { timing.ms_per_update = ms_per_update; }
-
-        var bodies = scene.bodies,
-            storage = engine.storage,
-            i, body;
-
-        function getEventArg1() {
-            return { engine: engine, scene: scene, timing: timing, bodies: bodies, storage: storage };
-        }
-
-        function getEventArg2() {
-            return { scene: scene, timing: timing, bodies: bodies, storage: storage, body: body, bodyIndex: i };
-        }
-
-        Events.trigger(engine, 'beforeTick', getEventArg1());
-
-        var isSceneUpdated = false;
-        while (timing.lag >= timing.ms_per_update) {
-            Events.trigger(scene, 'beforeUpdate', getEventArg1());
-            for (i = 0; i < bodies.length; i++) {
-                body = bodies[i];
-                Events.trigger(body, 'update', getEventArg2());
-            }
-            Events.trigger(scene, 'afterUpdate', getEventArg1());
-            timing.lag -= timing.ms_per_update;
-            isSceneUpdated = true;
-        }
-
-        if (isSceneUpdated) {
-            Events.trigger(scene, 'beforeRender', getEventArg1());
-            for (i = 0; i < bodies.length; i++) {
-                body = bodies[i];
-                Events.trigger(body, 'render', getEventArg2());
-            }
-            Events.trigger(scene, 'afterRender', getEventArg1());
-        }
-
-        Events.trigger(engine, 'afterTick', getEventArg1());
-
-        timing.previous = timing.current;
-        timing.current = undefined;
-        timing.elapsed = undefined;
-    };
-
-    Engine.start = function (engine, ms_per_update) {
         (function loop() {
-            Engine.update(engine, ms_per_update);
+            var time = Date.now();
+            var isUpdateRequired = (time > engine.time);
+
+            if (isUpdateRequired) {
+                Events.trigger(engine, 'beforeTick', engine);
+            }
+
+            while (time > engine.time) {
+                Events.trigger(engine, 'beforeUpdate', engine);
+                Events.trigger(engine, 'update', engine);
+                Events.trigger(engine, 'afterUpdate', engine);
+                engine.time += engine.ms_per_update;
+            }
+
+            if (isUpdateRequired) {
+                Events.trigger(engine, 'beforeRender', engine);
+                Events.trigger(engine, 'render', engine);
+                Events.trigger(engine, 'afterRender', engine);
+                Events.trigger(engine, 'afterTick', engine);
+            }
+
             engine.frameRequestId = window.requestAnimationFrame(function () { loop(); });
         })();
     };
 
     Engine.stop = function (engine) {
         window.cancelAnimationFrame(engine.frameRequestId);
-        Engine.resetTiming(engine);
         engine.frameRequestId = undefined;
-    };
-
-    Engine.addScene = function (engine, name) {
-        var newScene = {
-            timing: { previous: undefined, current: undefined, elapsed: undefined, lag: undefined, ms_per_update: undefined },
-            bodies: []
-        };
-
-        engine.scenes[name] = newScene;
-
-        return newScene;
-    };
-
-    Engine.resetTiming = function (engine) {
-        if (engine.sceneSwitch !== undefined) {
-            engine.scenes[engine.sceneSwitch].timing = { previous: undefined, current: undefined, elapsed: undefined, lag: undefined, ms_per_update: undefined }
-        };
-    };
-
-    Engine.switchScene = function (engine, name) {
-        Engine.resetTiming(engine);
-
-        if (engine.scenes[name] === undefined) {
-            Engine.addScene(engine, name);
-        }
-
-        engine.sceneSwitch = name;
-
-        return engine.scenes[name];
-    };
-
-    Engine.addBody = function (engine) {
-        var body = {};
-        engine.scenes[engine.sceneSwitch].bodies.push(body);
-        return body;
+        engine.time = undefined;
     };
 
 })();
